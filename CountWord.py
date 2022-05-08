2d3073af25210bb346b9beb6e884f239a155a723
TienThanhVu <tienthanhvu0204@gmail.com>
2022-05-08 15:19:36 +0700

    Đếm số từ

--- CountWord.py
+++ CountWord.py
@@ -6,18 +6,10 @@ def countWord (massage):
         key = word.lower ()
         key = key.strip ()
         if key in result.keys ():
-            result [key] = result [key] + 1
+            result [key] += 1
         else:
             result.update ({key: 1})
-        # if key in result.keys ():
-        #     result.get(key) + 1
-        # else:
-        #     result.update ({key : 0})
     return result
 massage = input ("input massage: ")
 text = countWord (massage)
 print (text)
-
-# result = {"I":"Tôi", "You":"Bạn"}
-# result.update ("I")
-# print (result)
